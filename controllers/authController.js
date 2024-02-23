ba0ef9a58a4fdf0ccf302a1ca4d2d055055d24d9
shivani <shivanicharhate03@gmail.com>
2024-02-23 23:02:46 +0530

    JWT Added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,26 +2,53 @@ import userModel from "../models/userModel.js";
 
 export const registerController = async (req, res, next) => {
   const { name, email, password } = req.body;
-
   if (!name) {
     next("Please provide name");
   }
-
   if (!email) {
     next("Please provide email");
   }
   if (!password) {
     next("Please provide password");
   }
-
   const existingUser = await userModel.findOne({ email });
   if (existingUser) {
     return res
       .status(200)
       .send({ message: "Email already register Please Login" });
   }
-
   const user = await userModel.create({ name, email, password });
-  res.status(201).send({ message: "New User register", newUser: user });
-  user;
+
+  const token = user.createJWT();
+  res.status(201).send({
+    message: "New User register",
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      location: user.location,
+    },
+
+    token,
+  });
+};
+
+export const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next("Please provide all fields");
+  }
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!email) {
+    next("Invalid userName , Password");
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!password) next("Invalid userName & Password");
+  user.password = undefined;
+  const token = user.createJWT();
+  res.status(200).json({
+    message: "Login Sucessfully",
+    user,
+    token,
+  });
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -28,5 +30,24 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// custome middelware for pw hashing
+
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// compare pw
+userSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+// JWT TOken
+userSchema.methods.createJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 export default mongoose.model("User", userSchema);
