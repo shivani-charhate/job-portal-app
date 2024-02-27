4468d23f32554816b8547488a4071d506a38a33c
shivani <shivanicharhate03@gmail.com>
2024-02-27 13:58:52 +0530

    update & delete Job

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -20,3 +20,37 @@ export const getAllJobs = async (req, res, next) => {
     jobs,
   });
 };
+
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  if (!company || !position) {
+    next("Please Provide All fields");
+  }
+  const job = await jobsModel.findById({ _id: id });
+  if (!job) {
+    next("No Jobs Found with this id");
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("You are not authorized person to update job");
+    return;
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ message: "Job Modify", updateJob });
+};
+
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const job = await jobsModel.findOne({ _id: id });
+  if (!job) {
+    next("No job found");
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("Not authorized to delete job");
+  }
+  await job.deleteOne();
+  res.status(200).json({ message: "Job deleted" });
+};
