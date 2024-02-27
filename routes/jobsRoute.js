7422636f8555e671af19f1d2d392101bf1294f53
shivani <shivanicharhate03@gmail.com>
2024-02-27 14:56:01 +0530

    update & delete Job

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -2,10 +2,14 @@ import express, { application } from "express";
 import {
   createJobController,
   getAllJobs,
+  updateJobController,
+  deleteJobController,
 } from "../controllers/jobsController.js";
 import userAuth from "../middelwares/authMiddelware.js";
 const router = express.Router();
 
 router.post("/create-job", userAuth, createJobController);
 router.get("/get-Jobs", userAuth, getAllJobs);
+router.put("/update-Job/:id", userAuth, updateJobController);
+router.delete("/delete-Job/:id", userAuth, deleteJobController);
 export default router;
